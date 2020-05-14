5982547a13f61b307ab03dd32e3654e158fc2bbe
Yukke2333 <65345664+Yukke2333@users.noreply.github.com>
2020-05-14 21:43:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,7 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+async def shingekin(ctx):
+    await ctx.send('はい、なります！w')
 
 bot.run(token)
